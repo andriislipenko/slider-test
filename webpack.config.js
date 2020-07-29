8fa646ee4aab260f141237b6907da4753c2b4d23
andriislipenko <andrii.slipenko@gmail.com>
2020-07-29 10:33:54 +0300

    from dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     module: {
         rules: [
